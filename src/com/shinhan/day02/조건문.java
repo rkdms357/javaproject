97bc5a1e237706d36ee9da80b076c11e97c42352
GAEUN JEONG <rkdms357@naver.com>
2025-10-27 08:57:14 +0900

    조건문

--- src/com/shinhan/day02/조건문.java
+++ src/com/shinhan/day02/조건문.java
@@ -3,102 +3,107 @@ package com.shinhan.day02;
 import java.util.Scanner;
 
 public class 조건문 {
-
     public static void main(String[] args) {
-        //f1();
-        //f2();
+        // f1();
+        // f2();
         //f3();
-        f4();
-        f5();
+        //f4();
+        //f5();
         f6();
     }
-
     private static void f6() {
-        int score;
-        String grade;
-        label35: {
-            grade = "B";
-            score = 0;
-            switch (grade) {
-                case "A":
-                    score = 100;
-                    break label35;
-                case "B":
-                    int result = 80;
-                    score = result;
-                    break label35;
+        String grade = "B";
+        int score = 0;
+        switch(grade) {
+            case "A":score =100;break;
+            case "B":{
+                int result = 100-20;
+                score=result;break;
             }
-            score = 60;
-        }
+            default:score=60;
 
-        int var10000;
-        label27: {
-            System.out.printf("11이전버전: 점수는 %d, 학점은 %s\n", score, grade);
-            switch (grade) {
-                case "A":
-                    var10000 = 100;
-                    break label27;
-                case "B":
-                    int result = 80;
-                    var10000 = result;
-                    break label27;
+        }
+        System.out.printf("11이전버전 : 점수는 %d, 학점은 %s\n", score, grade);
+
+        score = switch(grade) {
+            case "A" -> 100;
+            case "B" -> {
+                int result = 100-20;
+                yield result;
             }
-            var10000 = 60;
-        }
-        score = var10000;
-        System.out.printf("13버전부터: 점수는 %d, 학점은 %s\n", score, grade);
-    }
+            default -> 60;
 
-    private static void f5() {
-        int score = 100;
-        char score2 = 'A';
-        double c = 3.14;
-        String score3 = "A";
-        System.out.println(score3.hashCode());
+        };
+        System.out.printf("13버전부터 : 점수는 %d, 학점은 %s\n", score, grade);
     }
-
-    //Math.random() → 0.0 <  < 1.0 사이의 난수(double) 반환
+//    private static void f5() {
+//        int score = 100;
+//        char score2 = 'A';
+//        //switch문에서 ()의 결과가 실수 타입 불가
+//        double c = 3.14;
+//        String score3 = "A";
+//        switch(score3) {
+//        }
+//    }
     private static void f4() {
-        double a = Math.random();
+        //변이 6개인 주사위를 던져서 임의의 숫자가 나옴
+        double a = Math.random(); // 0<= <1
         System.out.println(a);
-        int b = (int)(Math.random()*(double)6.0F + (double)1.0F);
-        System.out.println(b);
-        int c = (int)(Math.random()*(double)45.0F) + 1; //로또 번호
+        int b = (int)(Math.random()*6+1);
+        System.out.println(b); //1<= <=6
+        //로또 번호를 무작위로 만들기 1<= <=45
+        int c = (int)(Math.random()*45)+1;
         System.out.println(c);
     }
-
     private static void f3() {
         Scanner sc = new Scanner(System.in);
         System.out.print("(switch)점수>>");
         int score = sc.nextInt();
         char grade;
+        // 점수에 따라 학점 부여
+        // 90이상이면 'A'학점
+        // 80이상이면 'B'
+        // 70이상이면 'C'
+        // 60이상이면 'D'
+        // 나머지는 'F'
+        // 가능: case 값1, 값2
         switch (score / 10) {
             case 10:
             case 9:
-                grade = 'A';
-                break;
+                grade = 'A'; break;
             case 8:
-                grade = 'B';
-                break;
+                grade = 'B'; break;
             case 7:
-                grade = 'C';
-                break;
+                grade = 'C'; break;
             case 6:
-                grade = 'D';
-                break;
+                grade = 'D'; break;
             default:
                 grade = 'F';
         }
-        System.out.printf("당신의 점수는 %d이고 학점은 %s\n", score, grade);
-        sc.close();
-        System.out.println("--------------------------");
-    }
+        System.out.printf("1.당신의 점수는 %d이고 학점은 %s\n", score, grade);
 
+        switch (score / 10) {
+            case 10,9 -> {grade='A';}
+            case 8 -> grade = 'B';
+            case 7 -> grade = 'C';
+            case 6 -> grade = 'D';
+            default -> grade = 'F';
+        }
+        System.out.printf("2.당신의 점수는 %d이고 학점은 %s\n", score, grade);
+        sc.close();
+        System.out.println("-------------------");
+    }
     private static void f2() {
         Scanner sc = new Scanner(System.in);
         System.out.print("점수>>");
         int score = sc.nextInt();
         char grade;
+        // 점수에 따라 학점 부여
+        // 90이상이면 'A'학점
+        // 80이상이면 'B'
+        // 70이상이면 'C'
+        // 60이상이면 'D'
+        // 나머지는 'F'
         if (score >= 90) {
             grade = 'A';
         } else if (score >= 80) {
@@ -110,22 +115,25 @@ public class 조건문 {
         } else {
             grade = 'F';
         }
-        System.out.printf("당신의 점수는 %d이고 학점은 %s", score, grade);
+        System.out.printf("당신의 점수는 %d이고 학점은 %s\n", score, grade);
         sc.close();
-        System.out.println("\n------------------------------");
+        System.out.println("-------------------");
     }
-
     private static void f1() {
+        // 표준입력(키보드)
         Scanner sc = new Scanner(System.in);
-        System.out.println("점수>>");
+        System.out.print("점수>>");
         int score = sc.nextInt();
+        // 점수가 70이상이면 합격
+        // 1.삼항연산자
         System.out.println(score >= 70 ? "합격" : "ㅠㅠ");
+        // 2.조건문
         if (score >= 70) {
             System.out.println("합격");
         } else {
-            System.out.println("ㅠㅠ");
+            System.out.println("불합격");
         }
         sc.close();
-        System.out.println("-----------------------");
+        System.out.println("-------------------");
     }
 }
