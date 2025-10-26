ec4715c4a2f87497281728a5978ba125530fca5c
GAEUN JEONG <rkdms357@naver.com>
2025-10-26 17:26:34 +0900

    LAB_DataTypeTest

---
+++ src/com/shinhan/day01/LAB_DataTypeTest.java
@@ -0,0 +1,14 @@
+package com.shinhan.day01;
+
+public class LAB_DataTypeTest {
+
+    public static void main(String[] args) {
+
+        int tom = -1;
+        char marry = '9';
+        boolean john = false;
+        String sarah = "Sarah Jang";
+        System.out.println( "our friends..\n"
+                + tom + ", " + marry + ", " + john + " and " + sarah);
+    }
+}
