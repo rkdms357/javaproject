d8244dc78d9616c6063227a3c5cf45d9bf765be8
GAEUN JEONG <rkdms357@naver.com>
2025-10-27 21:26:19 +0900

    LAB1_Q1

---
+++ src/com/shinhan/day03/LAB1_Q1.java
@@ -0,0 +1,22 @@
+package com.shinhan.day03;
+
+import java.util.Arrays;
+
+public class LAB1_Q1 {
+
+    public static void main(String[] args) {
+        int[] arr = {3, 24, 1, 55, 17, 43, 5};
+        for (int i = 0; i <= arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                int tmp = 0;
+                if (arr[j] < arr[i]) {
+                    tmp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = tmp;
+                }
+                continue;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
