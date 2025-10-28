841d34d036a9293d3a3904c9570437fe091c6b80
GAEUN JEONG <rkdms357@naver.com>
2025-10-28 21:27:49 +0900

    Calculator

---
+++ src/com/shinhan/day04/Calculator.java
@@ -0,0 +1,12 @@
+package com.shinhan.day04;
+
+public class Calculator {
+
+    int plus(int x, int y) {
+        return x + y;
+    }
+
+    int minus(int x, int y) {
+        return x - y;
+    }
+}
