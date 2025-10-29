c89e9bff758b1ef73596788b34b711692c994a1d
GAEUN JEONG <rkdms357@naver.com>
2025-10-29 23:17:59 +0900

    Printer

---
+++ src/com/shinhan/day05/q6/Printer.java
@@ -0,0 +1,17 @@
+package com.shinhan.day05.q6;
+
+public class Printer {
+
+    public static void println(int i) {
+        System.out.println(i);
+    }
+    public static void println(boolean b) {
+        System.out.println(b);
+    }
+    public static void println(double d) {
+        System.out.println(d);
+    }
+    public static void println(String s) {
+        System.out.println(s);
+    }
+}
---
+++ src/com/shinhan/day05/q6/PrinterTest.java
@@ -0,0 +1,12 @@
+package com.shinhan.day05.q6;
+
+public class PrinterTest {
+
+    public static void main(String[] args) {
+        //Printer printer = new Printer();
+        Printer.println(10);
+        Printer.println(true);
+        Printer.println(5.7);
+        Printer.println("홍길동");
+    }
+}
