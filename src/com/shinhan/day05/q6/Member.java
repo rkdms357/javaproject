253f0c6568c3e8573027367c60476015e48b862d
GAEUN JEONG <rkdms357@naver.com>
2025-10-29 23:17:42 +0900

    Member

---
+++ src/com/shinhan/day05/q6/Member.java
@@ -0,0 +1,14 @@
+package com.shinhan.day05.q6;
+
+public class Member {
+
+    String name;
+    String id;
+    String password;
+    int age;
+
+    public Member(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
+}
---
+++ src/com/shinhan/day05/q6/MemberService.java
@@ -0,0 +1,14 @@
+package com.shinhan.day05.q6;
+
+public class MemberService {
+
+    public boolean login(String id, String password) {
+        if(id.equals("hong") && password.equals("12345")){
+            return true;
+        } return false;
+    }
+
+    public void logout(String id) {
+        System.out.println(id + "님이 로그아웃되었습니다.");
+    }
+}
