43dcfccad92ff23dd0373cae4b30b5adf8c92f87
GAEUN JEONG <rkdms357@naver.com>
2025-11-01 16:35:41 +0900

    JDBC

---
+++ src/com/shinhan/day07/inter/JDBC.java
@@ -0,0 +1,10 @@
+package com.shinhan.day07.inter;
+
+public interface JDBC {
+    public abstract void connection();
+
+    public abstract void statement();
+
+    public abstract void execute();
+
+}
