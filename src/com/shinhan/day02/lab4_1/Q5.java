9fec27c5e9c032ba03f0d2e3c0e4ed9118bcfc27
GAEUN JEONG <rkdms357@naver.com>
2025-11-01 17:46:35 +0900

    LAB4_1_Q5

--- src/com/shinhan/day02/lab4_1/Q5.java
+++ src/com/shinhan/day02/lab4_1/Q5.java
@@ -1,10 +1,10 @@
-package com.shinhan.day02;
+package com.shinhan.day02.lab4_1;
 
-public class LAB1_Q5 {
+public class Q5 {
 
     public static void main(String[] args) {
-        for (int i = 0; i <= 10; i++) {
-            for (int j = 0; j < 10-i; j++) {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10 - i; j++) {
                 System.out.print("*");
             }
             System.out.println();
