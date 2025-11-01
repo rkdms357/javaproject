a769905175d4e61790d3fb3d49d361bc147bf063
GAEUN JEONG <rkdms357@naver.com>
2025-11-01 16:18:09 +0900

    SaveableInterface

---
+++ src/com/shinhan/day07/inter/SaveableInterface.java
@@ -0,0 +1,13 @@
+package com.shinhan.day07.inter;
+
+public interface SaveableInterface {
+
+    //1.상수
+    //2.추상메서드
+    //3.디폴트메서드
+    //4.static 메서드
+    //5.private 메서드
+    //6.private static 메서드
+
+    void save();
+}
