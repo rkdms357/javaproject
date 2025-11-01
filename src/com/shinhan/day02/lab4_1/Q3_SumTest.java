04e406af0f0f424978cc2746af9193c612a9f998
GAEUN JEONG <rkdms357@naver.com>
2025-11-01 17:26:10 +0900

    LAB4_1_Q3

--- src/com/shinhan/day02/lab4_1/Q3_SumTest.java
+++ src/com/shinhan/day02/lab4_1/Q3_SumTest.java
@@ -1,8 +1,22 @@
-package com.shinhan.day02;
+package com.shinhan.day02.lab4_1;
 
 import java.util.Scanner;
 
-public class LAB1_Q3_SumTest {
+public class Q3_SumTest {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("숫자를 입력하세요. : ");
+        int n = sc.nextInt();
+        int init = n % 2 == 0 ? 2 : 1;
+        int total = 0;
+        for (int i = init; i <= n; i += 2) {
+            total += i;
+        }
+        System.out.print("결과 값 : " + total);
+    }
+
+    /*
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("숫자를 입력하세요. : ");
@@ -22,4 +36,5 @@ public class LAB1_Q3_SumTest {
         }
         System.out.println("결과 값 : " + result);
     }
+     */
 }
