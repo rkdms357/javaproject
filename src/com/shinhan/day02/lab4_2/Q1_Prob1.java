193abea89d940dbd34da2fdfc29abd91d4558ec4
GAEUN JEONG <rkdms357@naver.com>
2025-11-01 18:03:14 +0900

    LAB4_2_Q1

--- src/com/shinhan/day02/lab4_2/Q1_Prob1.java
+++ src/com/shinhan/day02/lab4_2/Q1_Prob1.java
@@ -1,9 +1,24 @@
-package com.shinhan.day02;
+package com.shinhan.day02.lab4_2;
 
 import java.util.Scanner;
 
-public class LAB2_Q1_Prob1 {
+public class Q1_Prob1 {
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("1보다 크고 10보다 작은 정수를 입력하세요: ");
+        int dan = sc.nextInt();
+        if (dan < 2 || dan > 10) {
+            sc.close();
+            return;
+        }
+        for (int gop = 1; gop <= 9; gop++) {
+            System.out.printf("%d * %d = %d\n", dan, gop, dan * gop);
+        }
+        sc.close();
+    }
+
+    /*
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         while (true) {
@@ -21,4 +36,5 @@ public class LAB2_Q1_Prob1 {
         }
         sc.close();
     }
+     */
 }
