68a18d96ff2463f13fc0c8d1572366619b16a582
GAEUN JEONG <rkdms357@naver.com>
2025-11-02 20:57:15 +0900

    Radio

---
+++ src/com/shinhan/day07/lab8_1/Radio.java
@@ -0,0 +1,17 @@
+package com.shinhan.day07.lab8_1;
+
+public class Radio implements Volume {
+    int volLevel;
+
+    @Override
+    public void volumeUp(int level) {
+        volLevel += level;
+        System.out.println(getClass().getSimpleName() + "볼륨 올립니다." + volLevel);
+    }
+
+    @Override
+    public void volumeDown(int level) {
+        volLevel -= level;
+        System.out.println(getClass().getSimpleName() + "볼륨 내립니다." + volLevel);
+    }
+}
