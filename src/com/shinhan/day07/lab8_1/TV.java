11d2a1d4b02fabafbe924d33f50f66adf361679c
GAEUN JEONG <rkdms357@naver.com>
2025-11-02 20:57:31 +0900

    TV

---
+++ src/com/shinhan/day07/lab8_1/TV.java
@@ -0,0 +1,18 @@
+package com.shinhan.day07.lab8_1;
+
+public class TV implements Volume {
+    int volLevel;
+
+    @Override
+    public void volumeUp(int level) {
+        volLevel += level;
+        System.out.println(getClass().getSimpleName() + "볼륨 올립니다." + volLevel);
+    }
+
+    @Override
+    public void volumeDown(int level) {
+        volLevel -= level;
+        if (volLevel < 0) volLevel = 0;
+        System.out.println(getClass().getSimpleName() + "볼륨 내립니다." + volLevel);
+    }
+}
