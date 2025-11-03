a688b267229b9d681eca29ccd50e2d53986bc810
GAEUN JEONG <rkdms357@naver.com>
2025-11-04 08:47:16 +0900

    BonusPointAccount

---
+++ src/com/shinhan/day06/inheritence/BonusPointAccount.java
@@ -0,0 +1,25 @@
+package com.shinhan.day06.inheritence;
+
+import com.shinhan.day07.inter.Account;
+
+public class BonusPointAccount extends Account {
+    public int bonusPoint;
+
+    public BonusPointAccount(String accNo, String owner, int balance, int bonusPoint) {
+        super(accNo, owner, balance);
+        this.bonusPoint = bonusPoint;
+    }
+
+    @Override
+    public void deposite(int amount) { //변수 이름은 달라도 됨. 접근지정자는 더 넓어야됨
+        super.deposite(amount);
+        bonusPoint += (int) (amount * 0.001);
+    }
+
+    @Override
+    public String toString() {
+        String msg = super.toString();
+        return msg + "BonusPointAccount [bonusPoint=" + bonusPoint + "]";
+    }
+
+}
