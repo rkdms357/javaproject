402f1809f39a8181270b08dffc2a9ba67d3dc604
GAEUN JEONG <rkdms357@naver.com>
2025-11-03 22:02:26 +0900

    Book

---
+++ src/com/shinhan/day08/Book.java
@@ -0,0 +1,18 @@
+//package com.shinhan.day08;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//import lombok.ToString;
+//
+////Lombok라이브러리를 이용해서 컴파일시에 자동으로 메서드를 추가함
+//@ToString
+//@Getter
+//@Setter
+//@AllArgsConstructor
+//@NoArgsConstructor
+//public class Book {
+//    private String title;
+//    private int price;
+//}
---
+++ src/com/shinhan/day08/BookTest.java
@@ -0,0 +1,10 @@
+//package com.shinhan.day08;
+//
+//public class BookTest {
+//    public static void main(String[] args) {
+//        Book b1 = new Book();
+//        Book b2 = new Book("자바다", 50000);
+//
+//        System.out.println(b2);
+//    }
+//}
