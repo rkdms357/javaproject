ebc70134821bd7dc2d1665c92234357304b0944f
GAEUN JEONG <rkdms357@naver.com>
2025-11-04 08:48:02 +0900

    Employee

---
+++ src/com/shinhan/day05/lab6/Employee.java
@@ -0,0 +1,24 @@
+package com.shinhan.day05.lab6;
+public class Employee {
+    private String name;
+    private int age;
+    private String dept;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public String getDept() {
+        return dept;
+    }
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
+}
