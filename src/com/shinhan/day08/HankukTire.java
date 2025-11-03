e439de9ebd041b70b0be24444286a9fc534b4f9f
GAEUN JEONG <rkdms357@naver.com>
2025-11-03 21:46:16 +0900

    HankukTireTire

---
+++ src/com/shinhan/day08/HankukTire.java
@@ -0,0 +1,9 @@
+package com.shinhan.day08;
+
+public class HankukTire implements Tire {
+    @Override
+    public void roll() {
+        System.out.println("******************");
+        System.out.println("한국타이어가 굴러갑니다.");
+    }
+}
