3f798b9b6686dd86f828a97b4c257c2d3c231a0d
GAEUN JEONG <rkdms357@naver.com>
2025-11-05 21:03:20 +0900

    Car

---
+++ src/com/shinhan/day10/Car.java
@@ -0,0 +1,43 @@
+package com.shinhan.day10;
+
+import java.util.Objects;
+
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//import lombok.ToString;
+
+//@ToString
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Getter
+//@Setter
+public class Car {
+    String model;
+    int price;
+    String color;
+
+    Car(String model) {}
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(color, model, price);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Car other = (Car) obj;
+        return Objects.equals(color, other.color) && Objects.equals(model, other.model) && price == other.price;
+    }
+
+    String getCarInfo() {
+        return "카의 정보";
+    }
+}
